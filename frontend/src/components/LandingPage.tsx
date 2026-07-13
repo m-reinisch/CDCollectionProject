6f0d4b14eedb771b09c3b576949a5edbd1f0c7e6
m-reinisch <michael.reinisch@email.de>
2026-07-13 18:44:09 +0200

    startpage implemented

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,7 +1,18 @@
-export default function LandingPage() {
+type LandingPageProps = {
+    onGitHubLogin: () => void
+}
+
+export default function LandingPage(props: Readonly<LandingPageProps>) {
     return(
-        <div>
-            Startseite
+        <div className="landing-page">
+            <h2>Dies ist eine einfache Anwendung zum Verwalten Ihrer CD-Sammlung</h2>
+            <p>Eingeloggte Benutzer können eine (oder mehrere) Sammlung(en) anlegen,
+                CDs hinzufügen und in der Sammlung suchen.
+            </p>
+            <button id="gitbutton" onClick={props.onGitHubLogin}>
+                <span className="github" ></span>
+                <span>Mit GitHub einloggen</span>
+            </button>
         </div>
     )
 }
