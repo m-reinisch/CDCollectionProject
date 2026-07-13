63022cf92d8fcd323509e5427fba7aa1f3a52f0f
m-reinisch <michael.reinisch@email.de>
2026-07-13 16:20:33 +0200

    frontend development started

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Header from "./components/Header.tsx";
+import {Route, Routes} from "react-router-dom";
+import LandingPage from "./components/LandingPage.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
   return (
     <>
+      <Header key={"Kopf"} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+      <Footer key={"baseline"} />
     </>
   )
 }
